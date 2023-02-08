4c105bae95519ea471d69953310f9a88fd1e0d18
Chara White <chara@chara.pub>
2023-02-08 18:55:37 +0800

    change script

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ CPU_LIMIT_PRECENT=10
 
 command -v cpulimit >/dev/null || echo "Please install cpulimit."
 command -v cpulimit >/dev/null || exit 1
-echo "scale=99999999;4*a(1)" | bc -lq > /dev/null 2>/dev/null &
-PID=$!
-cpulimit -l $CPU_LIMIT_PRECENT -p $PID > /dev/null 2>/dev/null &
-echo "Calculating pi with ${CPU_LIMIT_PRECENT}% CPU limit..."
-echo "The PID is $PID"
+
+nohup cpulimit -l $CPU_LIMIT_PRECENT -- bash -c "while :;do a=1;done" >/dev/null 2>/dev/null
+
+echo "Doing something with ${CPU_LIMIT_PRECENT}% CPU limit..."
